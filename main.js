f3f9b3bb06c1f467572edb47c89f1d041b9725d9
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-16 15:18:19 +0900

    一つにまとめられる

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function message(){
+  {
   document.write("<h1 style='color: red;'>Welcome World");
+  document.write("<h1 style='color: blue;'>Welcome World");
+  document.write("<h1 style='color: green;'>Welcome World");
+  }
 }
