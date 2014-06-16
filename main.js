c1950983a2a51accf4ad512486e30b35e6688f64
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-16 15:22:22 +0900

    コメントの勉強

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function message(){
   {
-  document.write("<h1 style='color: red;'>Welcome World");
+  document.write("<h1 style='color: red;'>Welcome World"); //行末にもコメントできるよ
+  //コメントできるよ
+  /*
+  複数行も出来るよ
   document.write("<h1 style='color: blue;'>Welcome World");
   document.write("<h1 style='color: green;'>Welcome World");
+  */
   }
 }
